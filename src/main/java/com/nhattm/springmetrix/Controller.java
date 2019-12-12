07863d132033971e595797a03a9c0ddd85285964
nhattm2 <nhattm2@gmail.com>
2019-12-13 02:53:46 +0700

    kafka_test

--- src/main/java/com/nhattm/springmetrix/Controller.java
+++ src/main/java/com/nhattm/springmetrix/Controller.java
@@ -1,5 +1,7 @@
 package com.nhattm.springmetrix;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,10 +9,23 @@ import java.util.Date;
 
 @RestController
 public class Controller {
+
+    @Autowired
+    KafkaTemplate<String, String> kafkaTemplate;
+
+
     @GetMapping(value = "/test")
     public String test() {
         Date date = new Date();
         System.out.println("Hello" + new Date());
+        return date.toString();
+    }
+
+    @GetMapping(value = "/send")
+    public String send() {
+        Date date = new Date();
+        kafkaTemplate.send("baeldung","hehe");
+
         return date.toString();
     }
 }
