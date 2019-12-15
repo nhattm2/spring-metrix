9cf2712f994460ecfc605cd06336478537dcef5f
nhattm2 <nhattm2@gmail.com>
2019-12-15 22:03:56 +0700

    log

--- src/main/java/com/nhattm/springmetrix/Controller.java
+++ src/main/java/com/nhattm/springmetrix/Controller.java
@@ -1,8 +1,7 @@
 package com.nhattm.springmetrix;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
@@ -10,22 +9,46 @@ import java.util.Date;
 @RestController
 public class Controller {
 
-    @Autowired
-    KafkaTemplate<String, String> kafkaTemplate;
-
-
     @GetMapping(value = "/test")
     public String test() {
         Date date = new Date();
-        System.out.println("Hello" + new Date());
+        System.out.println("test" + new Date());
         return date.toString();
     }
 
-    @GetMapping(value = "/send")
-    public String send() {
+    @GetMapping(value = "/test1")
+    public void test1() {
         Date date = new Date();
-        kafkaTemplate.send("baeldung","hehe");
+        for (int i = 0; i < 1000000; i++) {
+            System.out.println("Test 1M: " + new Date());
+        }
+    }
 
-        return date.toString();
+    @GetMapping(value = "/test2")
+    public void test2() {
+        Date date = new Date();
+        for (int i = 0; i < 1000000000; i++) {
+            System.out.println("Test 1B: " + new Date());
+        }
+    }
+
+    @GetMapping(value = "/test3")
+    public void test3(@RequestParam(value = "sleep") int sleep) throws InterruptedException {
+        Date date = new Date();
+        for (int i = 0; i < 1000; i++) {
+            Thread.sleep(sleep);
+            for (int j = 0; j < 1000; j++) {
+                System.out.println("Test 1B: " + new Date());
+            }
+        }
+    }
+
+    @GetMapping(value = "/test4")
+    public void test4(@RequestParam(value = "time") int time, @RequestParam(value = "sleep") int sleep) throws InterruptedException {
+        Date date = new Date();
+        for (int i = 0; i < time; i++) {
+            Thread.sleep(sleep);
+            System.out.println("Test 1B: " + new Date());
+        }
     }
 }
